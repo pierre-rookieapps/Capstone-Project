2616a658eec4e72c6aa911d2d1e7c39ffe243dc6
pierre-rookieapps <pierre.rookieapps@gmail.com>
2023-11-06 21:45:30 +0100

    connected to mysql database and created models

--- Restaurant/models.py
+++ Restaurant/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
+class Book(models.Model):
+    name = models.CharField(max_length=255)
+    nb_of_guests = models.IntegerField(
+        default=1,
+        validators=[
+            MaxValueValidator(6),
+            MinValueValidator(1)
+        ]
+    )
+    booking_date = models.DateField()
+
+class Menu(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    inventory = models.IntegerField(
+        default=1,
+        validators=[
+            MaxValueValidator(5),
+            MinValueValidator(1)
+        ]
+    )
