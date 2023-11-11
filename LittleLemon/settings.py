85ce460e14b813317fc7b87550604939b136da77
pierre-rookieapps <pierre.rookieapps@gmail.com>
2023-11-11 16:16:24 +0100

    updated database paramaters

--- LittleLemon/settings.py
+++ LittleLemon/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'LittleLemon',
         'USER': 'root',
-        'PASSWORD': 'Pierrou_NZ14',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '3306',
         'OPTIONS': {
